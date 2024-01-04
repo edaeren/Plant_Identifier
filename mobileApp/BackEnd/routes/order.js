9930fdf584da5f374d0744688d5611cee32740f6
edaeren <edaeren2001@outlook.com>
2024-01-04 23:21:23 +0300

    deleted files

--- mobileApp/BackEnd/routes/order.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const orderController = require('../controllers/orderController');
-
-router.get('/:id',orderController.getUserOrders)
-
-module.exports = router;
--- mobileApp/BackEnd/routes/user.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const userController = require("../controllers/userController");
-
-router.delete('/:id',userController.deletUser);
-router.get('/:id',userController.getUser);
-
-module.exports = router;
